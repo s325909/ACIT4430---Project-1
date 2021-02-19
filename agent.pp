60980cdcbef932155df180ec8a926dd4d33bc362
Jaso Cyril <33516374+s325909@users.noreply.github.com>
2021-02-19 19:25:09 +0100

    update users in agent.pp

--- agent.pp
+++ agent.pp
@@ -26,12 +26,21 @@ service { "puppet" :
 	hasstatus => true,
 	require => Augeas['puppet.conf','puppet_default'],
 }
-user { "alice":
+user { "john":
         ensure => present,
         gid => '1002',
         home => '/home/john',
         shell => '/bin/bash',
         uid => '1002',
+	managehome => 'true',
+}
+user { "alice":
+        ensure => present,
+        gid => '1002',
+        home => '/home/alice',
+        shell => '/bin/bash',
+        uid => '1002',
+	managehome => 'true',
 }
 user { "bob":
         ensure => present,
@@ -39,6 +48,7 @@ user { "bob":
         home => '/home/bob',
         shell => '/bin/bash',
         uid => '1003',
+	managehome => 'true',
 }
 user { "kate":
         ensure => present,
@@ -46,4 +56,5 @@ user { "kate":
         home => '/home/kate',
         shell => '/bin/bash',
         uid => '1004',
+	managehome => 'true',
 }
