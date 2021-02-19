f4cf57d222ff059a5e791ce85a6e6d12c64c9908
Jaso Cyril <33516374+s325909@users.noreply.github.com>
2021-02-19 21:53:31 +0100

    update users.pp

--- users.pp
+++ users.pp
@@ -1,23 +1,23 @@
 user { "john":
-        ensure => 'present',
+        ensure => present,
         home => '/home/john',
         shell => '/bin/bash',
 	managehome => 'true',
 }
 user { "alice":
-        ensure => 'present',
+        ensure => present,
         home => '/home/alice',
         shell => '/bin/bash',
 	managehome => 'true',
 }
 user { "bob":
-        ensure => 'present',
+        ensure => present,
         home => '/home/bob',
         shell => '/bin/bash',
 	managehome => 'true',
 }
 user { "kate":
-        ensure => 'present',
+        ensure => present,
         home => '/home/kate',
         shell => '/bin/bash',
 	managehome => 'true',
