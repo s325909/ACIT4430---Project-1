3e9f09fcf6716c8f638bb809a6f5637c9be9ce1a
Jaso Cyril <33516374+s325909@users.noreply.github.com>
2021-02-19 19:56:28 +0100

    separate users.pp file

--- agent.pp
+++ agent.pp
@@ -26,35 +26,3 @@ service { "puppet" :
 	hasstatus => true,
 	require => Augeas['puppet.conf','puppet_default'],
 }
-user { "john":
-        ensure => present,
-        gid => '1002',
-        home => '/home/john',
-        shell => '/bin/bash',
-        uid => '1002',
-	managehome => 'true',
-}
-user { "alice":
-        ensure => present,
-        gid => '1002',
-        home => '/home/alice',
-        shell => '/bin/bash',
-        uid => '1002',
-	managehome => 'true',
-}
-user { "bob":
-        ensure => present,
-        gid => '1003',
-        home => '/home/bob',
-        shell => '/bin/bash',
-        uid => '1003',
-	managehome => 'true',
-}
-user { "kate":
-        ensure => present,
-        gid => '1004',
-        home => '/home/kate',
-        shell => '/bin/bash',
-        uid => '1004',
-	managehome => 'true',
-}
