516589626dce92b9dc1fde4a60bcf650a5d9c253
Jaso Cyril <33516374+s325909@users.noreply.github.com>
2021-02-27 16:23:48 +0100

    update desktop manifest and project.mln

--- manifest/desktop.pp
+++ manifest/desktop.pp
@@ -1,11 +1,11 @@
-$packages = ['ubuntu-desktop', 'vnc4server', 'jed']
-package { $packages:
-        ensure => installed
-}
-
 $users = ['john', 'alice', 'bob', 'kate']
 user { $users:
         ensure => present,
         shell => '/bin/bash',
         managehome => 'true',
+}
+
+$packages = ['ubuntu-desktop', 'vnc4server', 'jed']
+package { $packages:
+        ensure => installed
 }
