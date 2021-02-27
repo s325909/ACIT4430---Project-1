c2a7545fb7464b0640636f0e08dae8131979f84e
Jaso Cyril <33516374+s325909@users.noreply.github.com>
2021-02-27 12:51:08 +0100

    Create folder for puppet manifests

---
+++ manifest/desktop.pp
@@ -0,0 +1,30 @@
+package {'mongodb':
+        ensure => installed
+}
+package {'mongodb':
+        ensure => installed
+}
+user { "john":
+        ensure => present,
+        home => '/home/john',
+        shell => '/bin/bash',
+	managehome => 'true',
+}
+user { "alice":
+        ensure => present,
+        home => '/home/alice',
+        shell => '/bin/bash',
+	managehome => 'true',
+}
+user { "bob":
+        ensure => present,
+        home => '/home/bob',
+        shell => '/bin/bash',
+	managehome => 'true',
+}
+user { "kate":
+        ensure => present,
+        home => '/home/kate',
+        shell => '/bin/bash',
+	managehome => 'true',
+}
